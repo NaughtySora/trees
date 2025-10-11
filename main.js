e04c1c29bf1392945a695384964c1e838d24b7b7
Trofymenko Vladyslav <naughtysora@proton.me>
2025-10-11 20:07:45 +0300

    pre order traversal

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ class NodeLike {
   insert(value) {
     if (this.#root === null) return void (this.#root = this.#node(value));
     let queue = [this.#root];
+    // making queue with linkedlist to shift with no reindexing
     while (true) {
       const node = queue.shift();
       if (node.left === null) {
@@ -25,9 +26,35 @@ class NodeLike {
     queue = null;
   }
 
+  bft() { }
+  //sort, bst
+  #in() { }
+  // copy, build, serialize
+  #pre(callback) {
+    const stack = [];
+    let node = this.#root;
+    while (node) {
+      callback(node.value);
+      if (node.right !== null) stack.push(node.right);
+      node = node.left ?? stack.pop();
+    }
+  }
+  //delete, aggregate, evaluate
+  #post() { }
+  //     1
+  //    / \
+  //   2   3
+  //  / \
+  // 4   5
+  dft(method, callback) {
+    this.#traversal[method].call(this, callback);
+  }
+
   debug() {
     console.log(this.#root);
   }
+
+  #traversal = { pre: this.#pre, in: this.#in, post: this.#post };
 }
 
 class ArrayLike {
@@ -51,24 +78,35 @@ class BinaryTree {
   debug() {
     this.#representation.debug();
   }
+
+  dft(method, callback) {
+    this.#representation.dft(method, callback);
+  }
 }
 
 
 const tree = new BinaryTree();
 
+// tree.insert(1);
+// tree.debug();
+// tree.insert(2);
+// tree.debug();
+// tree.insert(3);
+// tree.debug();
+// tree.insert(4);
+// tree.debug();
+// tree.insert(5);
+// tree.debug();
+// tree.insert(6);
+// tree.debug();
+// tree.insert(7);
+// tree.debug();
+// tree.insert(8);
+// tree.debug();
+
 tree.insert(1);
-tree.debug();
 tree.insert(2);
-tree.debug();
 tree.insert(3);
-tree.debug();
 tree.insert(4);
-tree.debug();
 tree.insert(5);
-tree.debug();
-tree.insert(6);
-tree.debug();
-tree.insert(7);
-tree.debug();
-tree.insert(8);
-tree.debug();
+tree.dft('pre', console.log);
